33f15bc078ff16a800c008b537589379d2ae8035
codingshiksha <geekygautam1997@gmail.com>
2020-09-04 09:48:34 +0530

    second commit

--- Paytm/config.js
+++ Paytm/config.js
@@ -1,6 +1,6 @@
 var PaytmConfig = {
-  mid: "XXXXXXXXXXXXXXXXXXXX",
-  key: "XXXXXXXXXXXXXXXX",
-  website: "XXXXXXXXXX",
+  mid: "Kkatvd34334425459416",
+  key: "fgkmv_RHsi6R@QXm",
+  website: "WEBSTAGING",
 };
 module.exports.PaytmConfig = PaytmConfig;
